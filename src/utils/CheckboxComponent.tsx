04fc1126dbacae931bf9558f8f1b654e5fd4d489
ottonsam <ottonsam@gmail.com>
2023-03-03 23:12:31 -0300

    review show icons

--- src/utils/CheckboxComponent.tsx
+++ src/utils/CheckboxComponent.tsx
@@ -1,51 +1,105 @@
 import {
-  FormControl,
+  Checkbox,
   FormControlLabel,
-  FormLabel,
-  Radio,
-  RadioGroup,
+  FormGroup,
+  FormHelperText,
+  Stack,
+  Typography,
 } from "@mui/material";
-import { Controller, useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsEmojiHeartEyes,
+  BsFillEmojiSmileFill,
+  BsEmojiSmile,
+  BsFillEmojiNeutralFill,
+  BsEmojiNeutral,
+  BsFillEmojiFrownFill,
+  BsEmojiFrown,
+  BsFillEmojiAngryFill,
+  BsEmojiAngry,
+} from "react-icons/bs";
 
 const CheckboxComponent = () => {
-  const { control, watch } = useFormContext();
+  const {
+    watch,
+    setValue,
+    formState: { errors },
+  } = useFormContext();
   return (
-    <Controller
-      name="review"
-      control={control}
-      defaultValue={watch("review") || "fantastic"}
-      render={({ field }) => (
-        <FormControl>
-          <FormLabel>Avaliação</FormLabel>
-          <RadioGroup row value={watch("review") || undefined}>
-            <FormControlLabel
-              {...field}
-              value="fantastic"
-              control={<Radio />}
-              label="Fantástico"
+    <FormGroup>
+      <Stack flexDirection="row" justifyContent="center" gap={4}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={watch("review") === "very_unsatisfied"}
+              value="very_unsatisfied"
+              onChange={(e) => setValue("review", e.target.value)}
+              icon={<BsEmojiAngry size={40} />}
+              checkedIcon={<BsFillEmojiAngryFill size={40} />}
             />
-            <FormControlLabel
-              {...field}
-              value="good"
-              control={<Radio />}
-              label="Bom"
+          }
+          labelPlacement="bottom"
+          label={"Muito insatisfeito"}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={watch("review") === "unsatisfied"}
+              value="unsatisfied"
+              onChange={(e) => setValue("review", e.target.value)}
+              icon={<BsEmojiFrown size={40} />}
+              checkedIcon={<BsFillEmojiFrownFill size={40} />}
             />
-            <FormControlLabel
-              {...field}
-              value="ok"
-              control={<Radio />}
-              label="Normal"
+          }
+          labelPlacement="bottom"
+          label={"Insatisfeito"}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={watch("review") === "neutral"}
+              value="neutral"
+              onChange={(e) => setValue("review", e.target.value)}
+              icon={<BsEmojiNeutral size={40} />}
+              checkedIcon={<BsFillEmojiNeutralFill size={40} />}
             />
-            <FormControlLabel
-              {...field}
-              value="bad"
-              control={<Radio />}
-              label="Ruim"
+          }
+          labelPlacement="bottom"
+          label={"Normal"}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={watch("review") === "satisfied"}
+              value="satisfied"
+              onChange={(e) => setValue("review", e.target.value)}
+              icon={<BsEmojiSmile size={40} />}
+              checkedIcon={<BsFillEmojiSmileFill size={40} />}
             />
-          </RadioGroup>
-        </FormControl>
-      )}
-    />
+          }
+          labelPlacement="bottom"
+          label={"satisfeito"}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              value="very_satisfied"
+              onChange={(e) => setValue("review", e.target.value)}
+              icon={<BsEmojiHeartEyes size={40} />}
+              checkedIcon={<BsFillEmojiHeartEyesFill size={40} />}
+            />
+          }
+          labelPlacement="bottom"
+          label={"Muito satisfeito"}
+        />
+      </Stack>
+      <FormHelperText>
+        <Typography color="error" align="center">
+          {errors.review?.message?.toString()}
+        </Typography>
+      </FormHelperText>
+    </FormGroup>
   );
 };
 
