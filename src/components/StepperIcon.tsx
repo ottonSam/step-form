7d81eedfbc49be0bd4481eb18ad0ec8e79915768
Samuel Otton <ottonsam@gmail.com>
2023-04-11 16:29:23 -0300

    update: separated stepper icons

---
+++ src/components/StepperIcon.tsx
@@ -0,0 +1,41 @@
+import { Check } from "@mui/icons-material";
+import { StepIconProps, styled } from "@mui/material";
+
+const StepIconRoot = styled("div")<{ ownerState: { active?: boolean } }>(
+  ({ theme, ownerState }) => ({
+    color: theme.palette.mode === "dark" ? theme.palette.grey[700] : "#eaeaf0",
+    display: "flex",
+    height: 22,
+    alignItems: "center",
+    ...(ownerState.active && {
+      color: "#784af4",
+    }),
+    "& .QontoStepIcon-completedIcon": {
+      color: "#784af4",
+      zIndex: 1,
+      fontSize: 18,
+    },
+    "& .QontoStepIcon-circle": {
+      width: 8,
+      height: 8,
+      borderRadius: "50%",
+      backgroundColor: "currentColor",
+    },
+  })
+);
+
+function StepperIcon(props: StepIconProps) {
+  const { active, completed, className } = props;
+
+  return (
+    <StepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
+        <Check className="QontoStepIcon-completedIcon" />
+      ) : (
+        <div className="QontoStepIcon-circle" />
+      )}
+    </StepIconRoot>
+  );
+}
+
+export { StepperIcon };
