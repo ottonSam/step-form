e2f76c90e3812bf66e771ef2f507b759155613ad
Samuel Otton <ottonsam@gmail.com>
2023-04-12 15:11:36 -0300

    update: reset project using vite

--- src/data/DataCities.ts
+++
@@ -1,7 +0,0 @@
-const DataCities = [
-  { state: "Rio Grande do Norte", cities: ["Caicó", "São Fernando", "Natal"] },
-  { state: "Paraiba", cities: ["Campina Grande", "João Pessoa"] },
-  { state: "Ceara", cities: ["Fortaleza"] },
-];
-
-export default DataCities;
