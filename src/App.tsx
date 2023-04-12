ff590460b288752f626beb9560e3085953f46ebc
Samuel Otton <ottonsam@gmail.com>
2023-04-12 15:19:15 -0300

    feat: stepForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import StepForm from "./components/StepForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">step form clean</div>;
+  return <StepForm />;
 }
 
 export default App;
---
+++ src/components/StepForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const StepForm: React.FC = () => {
+  return <p>Step form component</p>;
+};
+
+export default StepForm;
