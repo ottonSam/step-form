3d0960454ff5e6bdb7d4b73b041c89b9251e7073
Samuel Otton <ottonsam@gmail.com>
2023-04-14 14:18:11 -0300

    update: subSubSteps

---
+++ src/components/StepForm/SubForms/SubForm1/SubSubForms/SubSubSubForms/SubSubSubForms1_1_1.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+// import { Container } from './styles';
+
+const SubSubSubForms1_1_1: React.FC = () => {
+  return <Typography>1.1.1</Typography>;
+};
+
+export default SubSubSubForms1_1_1;
---
+++ src/components/StepForm/SubForms/SubForm1/SubSubForms/SubSubSubForms/SubSubSubForms1_1_2.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+// import { Container } from './styles';
+
+const SubSubSubForms1_1_2: React.FC = () => {
+  return <Typography>1.1.2</Typography>;
+};
+
+export default SubSubSubForms1_1_2;
