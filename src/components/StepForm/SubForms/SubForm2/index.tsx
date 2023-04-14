d398230f7c47e59ad646073e27bbd56da7e6dfa8
Samuel Otton <ottonsam@gmail.com>
2023-04-14 14:17:17 -0300

    update: step two

---
+++ src/components/StepForm/SubForms/SubForm2/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+// import { Container } from './styles';
+
+const SubForm2: React.FC = () => {
+  return (
+    <Box>
+      <Typography>SubStep2</Typography>
+    </Box>
+  );
+};
+
+export default SubForm2;
